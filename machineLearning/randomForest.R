29fde40167e55b8d1b4970b1048b08a1fb1270e6
Carlos Ramirez <cramireza@ciencias.unam.mx>
2018-05-31 21:11:03 -0500

    random forest with cforest library

--- machineLearning/randomForest.R
+++ machineLearning/randomForest.R
@@ -11,7 +11,37 @@ data$Cell <- as.factor(data$Cell)
 #######################################################################
 ## cforest implementation of random forest
 ## One simple simulation with default values
+initialTime <- Sys.time()
 forest <- cforest(Cell~.,data=data)
 forest.pred <- predict(forest,newdata=data)
-confusionMatrix(forest.pred,data$Cell)
+onfusionMatrix(forest.pred,data$Cell)$overall[1]
+finalTime <- Sys.time() - initialTime
+finalTime
 
+######################################################################
+## performing parameter variation
+
+## number of simulations
+nsims <- 2
+## parameter to vary
+parameter <- seq(0.8,1,0.2)
+results <- matrix(0,nsims*length(parameter),2)
+colnames(results) <- c("accuracy","parameter")
+
+
+for (i in 1:length(parameter)){
+        for (j in 1:nsims) {
+                train <- createDataPartition(data$Cell,p=parameter,list=FALSE)
+                forest <- cforest(Cell~.,data=data,subset = train)
+                forest.pred <- predict(forest,newdata=data[-train,])
+                
+                ## index to store simulation data
+                index <- (nsims*(i-1)) + j
+                
+                ## storing results
+                results[index,1] <- confusionMatrix(forest.pred,
+                                                    data$Cell[-train])$overall[1]
+                results[index,2] <- parameter[i]
+        } 
+}
+results
