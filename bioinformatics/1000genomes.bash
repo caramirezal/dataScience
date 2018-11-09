7b0213f6cf103b4b0dfaa9b9e94254f5f113315d
caramirezal <cramireza@ciencias.unam.mx>
2018-11-09 10:07:06 -0600

    add coordinate range parameter

--- bioinformatics/1000genomes.bash
+++ bioinformatics/1000genomes.bash
@@ -14,5 +14,6 @@ cat data/1000genomes/igsr_samples.tsv | grep MXL | cut -f 1 > data/1000genomes/m
 
 ## Then filter vcf file by sample id using the next command
 ## As shown here: https://www.biostars.org/p/54641/
-./inst/vcftools_0.1.13/bin/vcftools --gzvcf data/1000genomes/ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --keep data/1000genomes/mxn_1000_genomes_list.txt --out mxn_genomes --plink
+./inst/vcftools_0.1.13/bin/vcftools --gzvcf data/1000genomes/ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --chr 6 --from-bp 1000000 --to-bp 2000000 --keep data/1000genomes/mxn_1000_genomes_list.txt --out mxn_genomes --plink
+
 
