def9a5ec899a8c019edc8f28b18c31e69d0f15e0
Carlos <caramireza@ciencias.unam.mx>
2019-07-27 22:21:57 -0500

    vector definition

--- courses/intro.R
+++ courses/intro.R
@@ -1,6 +1,31 @@
 ## Introduction to R Language
 
+## '##' As in bash corresponds to comments
+
+## Set the current working environment or directory
+setwd('~/sc/dataScience/courses/')
+
 ## Variable definition
-x1 <- 3.1416
-x2 = 0.999999
-str1 <- "Hola, mundo"
+x1 <- 3.1416               ## '<-' assigns a value to a variable
+print(x1)
+x2 = 0.999999              ## '=' is equivalent to '<-'
+x2
+str1 <- "Hola, mundo"      ## string variable
+cat(str1)
+z <- 1 + 1i                ## complex number 
+z
+l <- TRUE                  ## Logic variable
+l
+
+## vector definition
+vec1 <- c(1, 2, 3)         ## concatenation of variables
+vec1
+vec2  <- 1:3               ## vector of consecutive integers
+vec2
+vec3 <- c(vec1, 4)         ## concatenation of two vectors
+vec3
+vec4 <- c(TRUE, TRUE, FALSE)  ## logic vector concatenation
+vec4
+
+
+
