cb4cc9f8fdee05d8d073f23589aec46ac3ceb3b7
caramirezal <caramirezal@ciencias.unam.mx>
2019-12-05 16:37:03 +0100

    notes on processing sra files and conecting server

---
+++ bioinformatics/miscellanea.sh
@@ -0,0 +1,8 @@
+
+## open Rstudio inside conda environment
+open -a Rstudio
+
+## sending files to server
+scp \
+   data/SRR_Acc_List.txt \
+   bq_cramirez@???.uni-heidelberg.de:/home/bq_cramirez/data/SRR_Acc_List.txt
